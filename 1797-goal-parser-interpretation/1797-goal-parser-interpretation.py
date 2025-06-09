b8bdbaa1e7d6c87b0c5317c88044ebd9eab25412
YUGESHWARAN G <yugeshwarangm@gmail.com>
2025-06-09 21:00:38 +0530

    Time: 15 ms (68.51%), Space: 12.4 MB (77.62%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,6 +1,19 @@
 class Solution(object):
     def interpret(self, command):
-        # command.replace("()","o")
-        command=command.replace("()", "o").replace("(","").replace(")","")
-        print(command)
-        return command
+        o=False
+        ou=""
+        for c in command:
+            if c=="G":
+                ou+="G"
+            elif c=="(":
+                o=True
+            elif o==True and c==")":
+                ou+="o"
+                o=False
+            elif o==True and c=="a":
+                ou+="al"
+                o=False
+            else:
+                pass
+        print(ou)
+        return ou
