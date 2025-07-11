4be593b065d0dfc4830d28ca2f1b52532f6d8cc7
YUGESHWARAN G <yugeshwarangm@gmail.com>
2025-07-11 22:29:24 +0530

    Time: 427 ms (5%), Space: 13.1 MB (72.58%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        rl=len(matrix)
+        cl=len(matrix[0])
+        fr,fc=False,False
+        for i in range(rl):
+            for j in range(cl):
+                if(matrix[i][j]==0):
+                    if i==0:
+                        fr=True
+                    if j==0:
+                        fc=True
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        print(matrix)
+        for i in range(1,rl):
+            for j in range(1,cl):
+                if (matrix[i][0]==0 or matrix[0][j]==0):
+                    matrix[i][j]=0
+        print(matrix)
+        if fr:
+           for j in range(cl):
+                matrix[0][j]=0
+        if fc:
+            for i in range(rl):
+                matrix[i][0]=0
+        print(matrix)
+        return matrix
+
